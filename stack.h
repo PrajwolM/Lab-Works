aaa766cc463a825b9ece0ed5eadf785597581b80
PrajwolM <50495631+PrajwolM@users.noreply.github.com>
2019-11-13 20:46:28 -0800

    stack.h

---
+++ stack.h
@@ -0,0 +1,17 @@
+#define MAX_STACK_SIZE 100
+
+class Stack{
+    public:
+        Stack();
+        ~Stack();
+
+        void push(int element);
+        int pop();
+        int top();
+        bool isEmpty();
+        bool isFull();
+
+    private:
+        int elements[MAX_STACK_SIZE];
+        int top;
+};
