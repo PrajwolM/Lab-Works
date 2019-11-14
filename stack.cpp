30cb70a9fce94cc014bf7192ede64a5da608753b
PrajwolM <50495631+PrajwolM@users.noreply.github.com>
2019-11-13 20:47:00 -0800

    dgdsfg

---
+++ stack.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Stack.h"
+Stack::Stack(
+     top=-1;
+     )
+
+bool Stack::isFull(){return MAX_STACK_SIZE-1;}
+bool Stack::isEmpty(){return top<0;}
+
+Stack::~Stack(){}
+
+void Stack::push(int element)
+{
+    if(top >=MAX_STACK_SIZE - 1)//(isFull=1)
+    {
+        std::cout<<"Cannot push"<<element<<".Stack is full";
+    }
+    else
+    {
+        top++;
+        this->elements[top]=element;
+    }
+}
+
+int Stack::pop()
+{
+    if(top<0)//(isEmpty=1)
+    {
+        std::cout<<"Cannot pop an element from an empty stack."<<
+    }
+    else
+    {
+        //remove and return this ->elements[top];top--;
+        return elements[top--];
+    }
+}
+
+int Stack::top(){cout<<element[top];}
+
+int main()
+{
+Stack s;
+s.push(3);
+s.push(4);
+s.push(22);
+s.pop();
+s.top();
+}
